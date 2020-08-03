3ab9b49db2e10173b55a53d5672d58fcffe571e4
Sricharan <vodnalasricharan@gmail.com>
2020-08-03 17:27:55 +0530

    Update settings.py

--- HMS/settings.py
+++ HMS/settings.py
@@ -126,7 +126,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/staticfiles/'
 MEDIA_URL='/images/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    STATIC_ROOT
 ]
 MEDIA_ROOT = os.path.join(STATIC_ROOT, '/images')
 
