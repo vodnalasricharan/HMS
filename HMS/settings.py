c79d3a42784ab2733a90b0d7ab5989ab132df828
Sricharan <vodnalasricharan@gmail.com>
2020-08-03 17:48:22 +0530

    Update settings.py

--- HMS/settings.py
+++ HMS/settings.py
@@ -122,13 +122,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT='/static/'
-STATIC_URL = '/static/'
+STATIC_ROOT='/staticfiles/'
+STATIC_URL = '/staticfiles/'
 MEDIA_URL='/images/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+MEDIA_ROOT = os.path.join(STATIC_URL,'/images')
 
 
 # SMTP Configuration
