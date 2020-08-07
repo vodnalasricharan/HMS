26c527abd6f9b6b6b2f00f0cb5ecf9a82b769d82
Sricharan <vodnalasricharan@gmail.com>
2020-08-07 12:51:32 +0530

    added customs for heroku

--- HMS/settings.py
+++ HMS/settings.py
@@ -122,12 +122,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
-STATIC_ROOT='/staticfiles/'
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL='/images/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR,'static')
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 MEDIA_ROOT = os.path.join(BASE_DIR,'images')
 
 # SMTP Configuration
